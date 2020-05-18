3df38890b381afe8280a31bdd70205a4a7dbc75c
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-18 08:53:26 +0200

    Update background.sh

--- displaying-progress-spinner/background.sh
+++ displaying-progress-spinner/background.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 docker-compose up -d 1>&2
 docker exec -it moodle_moodle_1 mkdir -p /bitnami/moodle/moodledata/repository/docker
 BACKUP_MODDLE_FILE=$(ls -ail | grep backup | awk '{print $10}')
