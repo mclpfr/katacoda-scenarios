ae3b9afd0cb3031b24c99e89388b76c716775f4a
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-30 09:10:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 deploy_moddle()
 {
+  git clone https://github.com/mclpfr/moodle.git
+  cd moodle  
   docker-compose up -d 1>&2
   docker exec -it moodle_moodle_1 mkdir -p /bitnami/moodle/moodledata/repository/docker
   BACKUP_MODDLE_FILE=$(ls -ail | grep backup | awk '{print $10}')
