a6fd2a3987a4b8c324d7939154f9caa5a4fff3e3
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-31 10:06:07 +0200

    Update wait.sh

--- docker-training/assets/wait.sh
+++ docker-training/assets/wait.sh
@@ -4,11 +4,8 @@ deploy_moddle()
 {
   git clone https://github.com/mclpfr/moodle.git
   cd moodle 
-  echo "Avant docker-compose"
   docker-compose up -d 1>&2
-  echo "Apres docker-compose"
   sleep 240
-  echo "Avant docker mkdir"
   docker exec -it moodle_moodle_1 mkdir -p /bitnami/moodle/moodledata/repository/docker
   echo "Après docker mkdir"
   BACKUP_MODDLE_FILE=$(ls -ail | grep backup | awk '{print $10}')
