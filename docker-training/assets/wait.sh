15e3a6d48ea7c39682697e4e23e62e3c3f227581
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-31 10:00:48 +0200

    Update wait.sh

--- docker-training/assets/wait.sh
+++ docker-training/assets/wait.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+deploy_moddle()
+{
+  git clone https://github.com/mclpfr/moodle.git
+  cd moodle 
+  echo "Avant docker-compose"
+  docker-compose up -d 1>&2
+  echo "Apres docker-compose"
+  sleep 1
+  echo "Avant docker mkdir"
+  docker exec -it moodle_moodle_1 mkdir -p /bitnami/moodle/moodledata/repository/docker
+  echo "Après docker mkdir"
+  BACKUP_MODDLE_FILE=$(ls -ail | grep backup | awk '{print $10}')
+  docker cp $BACKUP_MODDLE_FILE moodle_moodle_1:/bitnami/moodle/moodledata/repository/docker
+  docker exec -it moodle_moodle_1 chown bitnami:daemon /bitnami/moodle/moodledata/repository/docker
+ }
+  
 show_progress()
 {
   #echo -n "Starting"
@@ -37,6 +53,7 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
+  deploy_moddle
   echo "Déploiement terminé"
 }
 
