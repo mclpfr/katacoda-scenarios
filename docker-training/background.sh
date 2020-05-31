b069f13ff1c478e88690b1b43229103b1a413e89
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-31 11:06:32 +0200

    Update background.sh

--- docker-training/background.sh
+++ docker-training/background.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+deploy_moddle()
+{
+  git clone https://github.com/mclpfr/moodle.git
+  cd moodle 
+  docker-compose up -d 1>&2
+ }
+ 
 sleep 1
 echo "done" >> /root/katacoda-finished
 sleep 5
+deploy_moddle >/dev/null 2>&1
 echo "done" >> /root/katacoda-background-finished
