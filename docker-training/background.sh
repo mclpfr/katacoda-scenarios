a54fe1589689735581e40231af16689ca4c4a150
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-31 09:43:15 +0200

    Update background.sh

--- docker-training/background.sh
+++ docker-training/background.sh
@@ -5,10 +5,14 @@ echo "done" >> /root/katacoda-finished
 deploy_moddle()
 {
   git clone https://github.com/mclpfr/moodle.git
-  cd moodle  
+  cd moodle 
+  echo "Avant docker-compose"
   docker-compose up -d 1>&2
+  echo "Apres docker-compose"
   sleep 240
+  echo "Avant docker mkdir"
   docker exec -it moodle_moodle_1 mkdir -p /bitnami/moodle/moodledata/repository/docker
+  echo "Après docker mkdir"
   BACKUP_MODDLE_FILE=$(ls -ail | grep backup | awk '{print $10}')
   docker cp $BACKUP_MODDLE_FILE moodle_moodle_1:/bitnami/moodle/moodledata/repository/docker
   docker exec -it moodle_moodle_1 chown bitnami:daemon /bitnami/moodle/moodledata/repository/docker
