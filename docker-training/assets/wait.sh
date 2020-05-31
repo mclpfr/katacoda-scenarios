0f94b0173a9b6ef77e0d1a6e15aea4c7ff58a268
mclpfr <45269208+mclpfr@users.noreply.github.com>
2020-05-31 14:11:55 +0200

    Update wait.sh

--- docker-training/assets/wait.sh
+++ docker-training/assets/wait.sh
@@ -16,7 +16,8 @@ restore_course()
   wget https://moodle.org/plugins/download.php/21420/moosh_moodle38_2020042300.zip
   unzip moosh_moodle38_2020042300.zip
   ln -s $PWD/moosh.php /usr/local/bin/moosh
-  moosh -n course-restore /bitnami/moodle/moodledata/repository/docker/backup-moodle2-course-59-d_-_module_12_-_docker-20200513-0726.mbz 1
+  cd /bitnami/moodle/moodledata/repository/docker
+  moosh -n course-restore backup-moodle2-course-59-d_-_module_12_-_docker-20200513-0726.mbz 1
  }
  
 show_progress()
